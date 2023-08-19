5c2936089ee6b5e4df1a10c7a6839e39e9f4c55f
rrdesh10 <51774394+rrdesh10@users.noreply.github.com>
2023-08-19 19:21:57 +0530

    added css, js styles

--- Space_Social/Space_Social/settings.py
+++ Space_Social/Space_Social/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIR = Path.joinpath(BASE_DIR, "static")
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, "static")]
 
 
 # Login
